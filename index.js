4049bdd3ef5dad85f033fd8bec29f6e43f59d296
Joel Ortiz <l3go.joo@gmail.com>
2024-07-04 21:22:57 +0100

    basic functionality

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
     "/"];
+
+const genPasswordsBtn = document.getElementById("gen-passwords-btn")
+const passwordEl1 = document.getElementById("password-el-1")
+const passwordEl2 = document.getElementById("password-el-2")
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length)
+}
+
+function generatePassword(length = 15) {
+    let password = ""
+    for (i = 0; i <= length; i++) {
+        password += characters[getRandomIndex(characters)]
+    }
+    return password
+}
+
+genPasswordsBtn.addEventListener("click", function () {
+    passwordEl1.textContent = generatePassword()
+    passwordEl2.textContent = generatePassword()
+})
