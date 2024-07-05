bf35c9e119f68561c86bb4ea4e6d1de96b3442eb
Joel Ortiz <l3go.joo@gmail.com>
2024-07-05 01:04:49 +0100

    numbers and symbols toggles

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-    "/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 const genPasswordsBtn = document.getElementById("gen-passwords-btn")
 const lengthInput = document.getElementById("length-input")
 const passwordElements = document.querySelectorAll(".password-el")
+const numbersCheckbox = document.getElementById("numbers-checkbox")
+const symbolsCheckbox = document.getElementById("symbols-checkbox")
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length)
 }
 
 function generatePassword(length) {
+    let passwordCharacters = characters
+    if (numbersCheckbox.checked) {
+        passwordCharacters = passwordCharacters.concat(numbers)
+    }
+    if (symbolsCheckbox.checked) {
+        passwordCharacters = passwordCharacters.concat(symbols)
+    }
+
     let password = ""
     for (i = 0; i < length; i++) {
-        password += characters[getRandomIndex(characters)]
+        password += passwordCharacters[getRandomIndex(passwordCharacters)]
     }
     return password
 }
